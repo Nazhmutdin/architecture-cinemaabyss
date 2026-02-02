d6f2402283e60a7e2dc65d0f03b6c414c1b7975d
Nazhmutdin <gumuevnazhmutdin1248@gmail.com>
2026-02-02 15:01:51 +0300

    fix

--- src/microservices/proxy/main.py
+++ src/microservices/proxy/main.py
@@ -23,11 +23,11 @@ async def proxy_fallback(request: Request, path: str):
     if "api/movies" in path:
         use_new = GRADUAL and random.randint(1, 100) <= PERCENT
         target = MOVIES_SERVICE_URL if use_new else MONOLITH_URL
-        url = f"{target}/api/movies/{path}"
+        url = f"{target}/{path}"
     elif "api/events" in path:
         use_new = GRADUAL and random.randint(1, 100) <= PERCENT
         target = EVENTS_SERVICE_URL if use_new else MONOLITH_URL
-        url = f"{target}/api/events/{path}"
+        url = f"{target}/{path}"
     else:
         url = f"{MONOLITH_URL}/{path}"
 
